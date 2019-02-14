3e1f8edc185dbe097a7728f775f8861a513ce0ef
Sol1323 <solimar.bp90@gmail.com>
2019-02-13 23:18:31 -0800

    added custom props to TodoList component

--- hello-2/hello.jsx
+++ hello-2/hello.jsx
@@ -12,15 +12,18 @@ class TodoList extends React.Component {
   render() {
     return <div>
       <h1>TODO LIST</h1>
-      <TodoItem item="buy groceries" />
-      <TodoItem item="laundry" />
-      <TodoItem item="clean house"/>
+      <TodoItem item="buy groceries" important={true} />
+      <TodoItem item="laundry" important={false} />
+      <TodoItem item="clean house" important={true} />
     </div>
   }
 }
 
 class TodoItem extends React.Component {
   render() {
-    return<li> {this.props.item} </li>
+      if (this.props.important){
+        return <li><b>{this.props.item}</b></li>
+      }
+      return <li>{this.props.item}</li>
   }
 }
